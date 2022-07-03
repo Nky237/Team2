80687c49d71bc5731505c359b53df0a2b7e241f4
Ezike Nkemdili Jane <ezikenkemdilijane@gmail.com>
2022-07-03 22:01:27 +0100

    new commit

--- index.js
+++ index.js
@@ -1,53 +1,82 @@
 const selectElement = (selector) => {
-    const element = document.querySelector(selector);
-    if (element) return element;
-    throw new Error(`Cannot find the element ${selector}`);
-  };
+  const element = document.querySelector(selector);
+  if (element) return element;
+  throw new Error(`Cannot find the element ${selector}`);
+};
 
 
-
-
+// Select Items
 const form = selectElement("form");
 const input = selectElement("input");
-const result = selectElement(".result"); 
+const result = selectElement(".list"); 
 let navLinks = document.querySelector('.mobile-nav-links')
 let burgerBtn = document.querySelector('.burger')
 let mainContent = document.querySelector('main')
-burgerBtn.addEventListener('click',function(){
-    navLinks.classList.toggle('active')
-    mainContent.classList.toggle('main-drop')
-})
+let errorMsg = document.querySelector('.msg')
 
+
+// DROPDOWN MENU
+burgerBtn.addEventListener('click',function(){
+  navLinks.classList.toggle('active')
+  mainContent.classList.toggle('main-drop')
+})
 
 
 // SHORTENING
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const url = input.value;
-  
-    shortenUrl(url);
-  });
-  
-  async function shortenUrl(url) {
-    try {
-      const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`);
-      const data = await res.json();
-      const newUrl = document.createElement("div");
-      newUrl.classList.add("item");
-      newUrl.innerHTML = ` 
-     <p> ${data.result.short_link}</p>
-     <button class="newUrl-btn" >Copy</button>
-     `;
-      result.prepend(newUrl);
-      const copyBtn = result.querySelector(".newUrl-btn");
-      copyBtn.addEventListener("click", () => {
-        navigator.clipboard.writeText(copyBtn.previousElementSibling.textContent);
-      });
-      input.value = "";
-    } catch (err) {
-      console.log(err);
-    }
-  }
-  
+e.preventDefault();
+if(input.value === ''){
 
+   // Error Message
+  errorMsg.innerHTML = 'Please add a link';
+  input.style.border = '1px solid rgb(250, 70, 70)';
+  
+    // Revert to normal
+  setTimeout(()=>{
+  errorMsg.innerHTML = '';
+  input.style.border = 'none';
+  }, 2000);
+
+}else{
+const url = input.value;
+
+shortenUrl(url);
+}
+});
+
+// Fetch API
+async function shortenUrl(url) {
+try {
+  const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`);
+  const data = await res.json();
+  const newUrl = document.createElement("p");
+  newUrl.classList.add("list-group");
+  newUrl.innerHTML = ` 
+ <p> ${data.result.short_link}</p>
+ <button class="btn-list" >Copy</button>
+ `;
+  result.prepend(newUrl);
+
+  // Copy-Button Interaction
+  const copyBtn = result.querySelector(".btn-list");
+
+  copyBtn.addEventListener("click", (e) => {
+    if(e.target.classList.contains('btn-list')){
+      navigator.clipboard.writeText(copyBtn.previousElementSibling.textContent);
+      let btn = e.target;
+      btn.style.backgroundColor = 'rgb(61, 3, 95)';
+      btn.innerHTML = 'Copied!'
+
+      // Revert to normal
+      setTimeout(() =>{
+          btn.style.backgroundColor = '';
+          btn.innerHTML = 'Copy'
+      }, 1000);
+    }
+  });
+  input.value = "";
+} catch (err) {
+  console.log(err);
+}
+}
